66ea4e23ce2b2f1c2a52cb9a0ab7c9b0933600e7
Luis Zubia <soyzubia@gmail.com>
2023-09-25 12:18:12 -0700

    'schema' after all 'posts' migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_190533) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_191550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_190533) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "comments_counter", default: 0, null: false
+    t.bigint "likes_counter", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
