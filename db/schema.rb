fa10bf4415a72e54408fbb5a550afdba4a59e3a0
Luis Zubia <soyzubia@gmail.com>
2023-09-27 08:37:09 -0700

    'schema' after adding 'foreign key' 'author' migration to 'posts' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_135016) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_141844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_135016) do
     t.text "text", null: false
     t.bigint "comments_counter", default: 0, null: false
     t.bigint "likes_counter", default: 0, null: false
+    t.bigint "author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_135016) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
