ab356390c07ad86b296121f051ceb417ea3aab7d
Luis Zubia <soyzubia@gmail.com>
2023-09-27 15:52:11 -0700

    Add #update_comments_counter at 'comment' model, which is executed after a comment is created or destroyed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ApplicationRecord
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
   # associations
   belongs_to :user
   belongs_to :post, counter_cache: :comments_counter
+
+  def update_comments_counter
+    post.comments_counter = post.comments.count
+  end
 end
