384bc8a3bb7f5359770bffef3ef4dd5f65153563
Luis Zubia <soyzubia@gmail.com>
2023-09-27 16:08:55 -0700

    Add #update_likes_counter at 'like' model, which is executed after a like is created or destroyed

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,12 @@
 class Like < ApplicationRecord
+  after_create :update_likes_counter
+  after_destroy :update_likes_counter
+
   # associations
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
+
+  def update_likes_counter
+    post.likes_counter = post.likes.count
+  end
 end
