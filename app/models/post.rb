7a5eb7653bf0aefea7b2f4c0ceca0e2d7c4c29ef
Luis Zubia <soyzubia@gmail.com>
2023-09-27 15:15:44 -0700

    Add #most_recent_comments method, which returns by default the 5 most recent comments for a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', inverse_of: 'posts', counter_cache: :posts_counter
   has_many :comments
   has_many :likes
+
+  def most_recent_comments(number = 5)
+    comments.limit(number).order(created_at: :desc)
+  end
 end
