369bed69e2ca3cc1a209a5a6383143ff0ee9ece7
Luis Zubia <soyzubia@gmail.com>
2023-09-27 11:54:54 -0700

    'schema' after removing 'foreign key' 'author_id' at 'posts' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_141844) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_141844) do
     t.text "text", null: false
     t.bigint "comments_counter", default: 0, null: false
     t.bigint "likes_counter", default: 0, null: false
-    t.bigint "author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,5 +55,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_141844) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id"
 end
