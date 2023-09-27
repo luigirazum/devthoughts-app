fc05f9248ad9b41ae317c55347bdcb5db8caad07
Luis Zubia <soyzubia@gmail.com>
2023-09-27 15:11:52 -0700

    Add #most_recent_posts method, which returns by default the 3 most recent posts for an author

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', inverse_of: 'author'
   has_many :likes
   has_many :comments
+
+  def most_recent_posts(num = 3)
+    posts.limit(num).order(created_at: :desc)
+  end
 end
