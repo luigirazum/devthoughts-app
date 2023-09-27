f2f4e9364aa5c2425a55b237b23664c77b1e6658
Luis Zubia <soyzubia@gmail.com>
2023-09-27 12:14:23 -0700

    Final 'schema' after adding 'tables', 'columns', 'references' and, 'indexes' to the 'data model'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_185521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "text", null: false
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -27,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
     t.text "text", null: false
     t.bigint "comments_counter", default: 0, null: false
     t.bigint "likes_counter", default: 0, null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_180846) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
