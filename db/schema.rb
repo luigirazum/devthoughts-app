eff8f4ebd105771130033e335d5ac65d6ae7a4ce
Luis Zubia <soyzubia@gmail.com>
2023-09-27 06:54:22 -0700

    'schema' after adding 'references' migrations to 'comments' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_191550) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_134608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_191550) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "text", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -43,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_191550) do
     t.bigint "posts_counter", default: 0, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
