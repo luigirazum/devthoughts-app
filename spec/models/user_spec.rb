674f5ff6d3fc52269d43afead01fdfeeb4119fba
Luis Zubia <soyzubia@gmail.com>
2023-09-28 03:36:05 -0700

    Add specs for: 'User', to: check for '.name' is provided and is 5 to 50 characters length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe User, type: :model do
   subject(:user) { described_class.create(name: 'Name LastName') }
 
@@ -13,6 +12,24 @@ RSpec.describe User, type: :model do
     it '- with valid attributes' do
       expect(user).to be_valid
       expect(attr_mod({ name: nil })).to_not be_valid
+    end
+  end
+
+  describe '* attributes' do
+    describe '.name validations' do
+      it '- must be provided' do
+        expect(attr_mod({ name: nil })).to_not be_valid
+      end
+
+      it '- must be at least 5 characters' do
+        expect(attr_mod({ name: 'a' * 4 })).to_not be_valid
+      end
+
+      it '- must be 50 characters max' do
+        expect(attr_mod({ name: 'a' * 52 })).to_not be_valid
+      end
+    end
+
     end
   end
 end
