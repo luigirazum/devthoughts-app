9c2ee1878df724283450e9afe0ee3b4bed79dc7c
Luis Zubia <soyzubia@gmail.com>
2023-09-28 02:30:19 -0700

    Add validations to 'user', 'uniqueness/scope', a 'user' can give a like to a 'post' just once

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   after_create :update_likes_counter
   after_destroy :update_likes_counter
 
+  # validations
+  validates :user, uniqueness: { scope: :post, message: "can't like a post more than once" }
+
   # associations
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
