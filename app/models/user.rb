f02b0da91468ef9d89155e04babbe27022158512
Luis Zubia <soyzubia@gmail.com>
2023-09-28 01:27:02 -0700

    Add validations to 'posts_counter', 'numericality/only_integer' and 'numericality/gt_or_eq_to_0', should be an integer greater than or equal to zero

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # validations
   validates :name, presence: { message: "can't be blank" }
-  validates :name, length: { in: 5..50, message: "must be 5 to 50 characters"}
+  validates :name, length: { in: 5..50, message: 'must be 5 to 50 characters' }
+  validates :posts_counter, numericality: { only_integer: true, message: 'must be an integer number' }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, message: "can't be negative" }
 
   # associations
   has_many :posts, foreign_key: 'author_id', inverse_of: 'author'
