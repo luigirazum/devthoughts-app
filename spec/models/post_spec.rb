8944501e4484d0868d7774ac6bc2a3c8e42f3a7f
Luis Zubia <soyzubia@gmail.com>
2023-09-28 10:10:03 -0700

    Add specs for: 'Post', to: check for '#most_recent_comments' method, it should return the five most recent 'comments' for a given post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -64,4 +64,28 @@ RSpec.describe Post, type: :model do
       end
     end
   end
+
+  describe '* methods' do
+    describe '#most_recent_comments' do
+      context "- when a 'Post' is created" do
+        it "> should return zero 'comments'" do
+          expect(post.most_recent_comments).to be_empty
+        end
+      end
+
+      context "- when a 'Post' has several 'comments'" do
+        it "> should return the five most recent 'comments'" do
+          comments = []
+          %i[1 2 3 4 5 6 7 8].each do |comment_number|
+            time = Time.now - (2.hour * comment_number.to_s.to_i)
+            user = User.create(name: "User ##{comment_number} Name")
+            comments << Comment.create(user:, post:, text: "Text for Comment ##{comment_number}", created_at: time, updated_at: time)
+          end
+
+          comment1, comment2, comment3, comment4, comment5, = comments
+          expect(post.most_recent_comments).to eq([comment1, comment2, comment3, comment4, comment5])
+        end
+      end
+    end
+  end
 end
