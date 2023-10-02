fe335a37967737c757d57ecfbc10d43a7922408b
Luis Zubia <soyzubia@gmail.com>
2023-10-02 13:49:40 -0700

    Add 'tag's to the different groups of tests - :index to run the tests for 'GET /index' requests - :show to run the tests for 'GET /show' requests - :status to run the tests to check response 'status' - :template to run the tests to check the rendered 'template' - :placeholder to runt the tests to check the 'placeholder' showed
    To use this tags you can run
    rspec spec/requests[/file_name.rb] --tag tag_name

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "'Posts' - [Controller]", type: :request do
-  describe "'GET /index' => 'index' action at 'posts' controller" do
+  describe "'GET /index' => 'index' action at 'posts' controller", :index do
     before { get user_posts_path({ user_id: 1 }) }
 
-    context "* 'status'" do
+    context "* 'status'", :status do
       it '- returns http success' do
         expect(response).to have_http_status(:success)
       end
     end
 
-    context "* 'template'" do
+    context "* 'template'", :template do
       it "- renders 'index' template" do
         expect(response).to render_template(:index)
         expect(response).to render_template('index')
@@ -21,23 +21,23 @@ RSpec.describe "'Posts' - [Controller]", type: :request do
       end
     end
 
-    context "* 'placeholder text'" do
+    context "* 'placeholder text'", :placeholder do
       it "- 'body' includes 'Posts for a User'" do
         expect(response.body).to match(/Posts for a User/)
       end
     end
   end
 
-  describe "'GET /show' => 'show' action at 'posts' controller" do
+  describe "'GET /show' => 'show' action at 'posts' controller", :show do
     before { get user_post_path({ user_id: 1, id: 1 }) }
 
-    context "* 'status'" do
+    context "* 'status'", :status do
       it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
     end
 
-    context "* 'template'" do
+    context "* 'template'", :template do
       it "- renders 'show' template" do
         expect(response).to render_template(:show)
         expect(response).to render_template('show')
@@ -48,7 +48,7 @@ RSpec.describe "'Posts' - [Controller]", type: :request do
       end
     end
 
-    context "* 'placeholder text'" do
+    context "* 'placeholder text'", :placeholder do
       it "- 'body' includes 'Details for a given Post'" do
         expect(response.body).to match(/Details for a given Post/)
       end
