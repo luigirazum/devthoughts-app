904a0a94af5c05311cd5cb45d3c615f279a1ac05
Luis Zubia <soyzubia@gmail.com>
2023-10-05 09:36:14 -0700

    Add code to create a post using the form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @current_user = current_user
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: } }
+    end
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :text))
+    post.author = current_user
+    respond_to do |format|
+      format.html do
+        if post.save
+          # success message
+          flash[:success] = 'Post published successfully'
+          # redirect to index
+          redirect_to user_posts_path
+        else
+          # error message
+          flash.now[:error] = 'Error: Post could not be published'
+          # render new
+          render :new, locals: { post: }
+        end
+      end
+    end
+  end
 end
