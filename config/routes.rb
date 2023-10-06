30911d1671aa7a9316a8003456ed063ce90b7331
Luis Zubia <soyzubia@gmail.com>
2023-10-05 18:41:02 -0700

    Match the routes for 'posts#new', 'comments#new', 'likes#create'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'likes/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   constraints(id: /\d+/, user_id: /\d+/, author_id: /\d+/) do
+    resources :posts, only: %i[new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[create]
+    end
     resources :users, only: %i[index show] do
-      resources :posts, only: %i[index show new create] do
-        resources :comments, only: %i[new create]
-        resources :likes, only: %i[create]
-      end
+      resources :posts, only: %i[index show]
     end
   end
 
