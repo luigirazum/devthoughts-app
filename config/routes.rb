1e5a9670d3448f80904efcf37be9ac9b0f721c70
binyamT <binyampowerhc@gmail.com>
2023-10-11 11:37:07 +0300

    add the routes for the devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: 'auth',
+                     path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
+                                   confirmation: 'verification', unlock: 'unblock', registration: 'register',
+                                   sign_up: 'cmon_let_me_in' }
 
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    root to: 'devise/sessions#new'
+  end
   constraints(id: /\d+/, user_id: /\d+/, author_id: /\d+/) do
     resources :posts, only: %i[new create] do
       resources :comments, only: %i[new create]
@@ -12,7 +18,7 @@ Rails.application.routes.draw do
     end
   end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'users#index'
+  # # Defines the root path route ("/")
+  # # root "articles#index"
+  # root 'users#index'
 end
