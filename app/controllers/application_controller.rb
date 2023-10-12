5a68407d6c00ca17f66bfeae55c4a242b9b1bb46
Luis Zubia <soyzubia@gmail.com>
2023-10-12 03:03:16 -0700

    Add helper method for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_user
-  def after_sign_in_path_for(_resource)
-    users_path
-  end
+
+  # def after_sign_in_path_for(_resource)
+  #   users_path
+  # end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
   end
 end
